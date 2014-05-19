85967c03d9ac2ff18e11d7992dfe99058eb07040
brookebc <brooke.casey@gmail.com>
2014-05-19 14:10:30 -0400

    clean angular repo for my shopping cart app

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('gitShoppingApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
